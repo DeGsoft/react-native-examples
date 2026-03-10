377929ac152e7e26b060c84b39b59fdf1dde85ca
DeGsoft <diegoezequielguillen@gmail.com>
2026-03-10 10:36:45 -0300

    feat (useAppState): Refetch on App focus

--- TanStackQuery/App.tsx
+++ TanStackQuery/App.tsx
@@ -1,18 +1,29 @@
 import {
+  focusManager,
   QueryClient,
   QueryClientProvider,
   useQuery,
 } from '@tanstack/react-query';
 import React from 'react';
-import { Image, StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
+import { AppStateStatus, Image, Platform, StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
 import { useOnlineManager } from './src/hooks/useOnlineManager';
+import { useAppState } from './src/hooks/useAppState';
+
+function onAppStateChange(status: AppStateStatus) {
+  // React Query already supports in web browser refetch on window focus by default
+  if (Platform.OS !== 'web') {
+    focusManager.setFocused(status === 'active')
+  }
+}
 
 function App() {
   useOnlineManager();
+  useAppState(onAppStateChange);
+
   const isDarkMode = useColorScheme() === 'dark';
   const queryClient = new QueryClient();
 
